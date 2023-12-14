006875b9d95b4a53365731d51bb646ea41a3e834
arfin <developerarfin@gmail.com>
2023-12-14 20:11:52 +0600

    Fixed gradle build failed issues

--- app/src/main/java/com/example/coino/feature_search/domain/model/SearchCoins.kt
+++ app/src/main/java/com/example/coino/feature_search/domain/model/SearchCoins.kt
@@ -1,5 +1,9 @@
 package com.example.coino.feature_search.domain.model
 
+import androidx.compose.runtime.Immutable
+
+
+@Immutable
 data class SearchCoins(
     val api_symbol: String,
     val id: String,
