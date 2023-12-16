e6cd129987e8854f24fe7b6a716e09c391aead4a
arfin <developerarfin@gmail.com>
2023-12-16 12:43:30 +0600

    Line Chart Improved

--- app/src/main/java/com/example/coino/feature_coins/presentation/components/LineChart.kt
+++ app/src/main/java/com/example/coino/feature_coins/presentation/components/LineChart.kt
@@ -1,22 +1,24 @@
 package com.example.coino.feature_coins.presentation.components
 
+import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import co.yml.charts.axis.AxisData
+import co.yml.charts.common.model.AccessibilityConfig
 import co.yml.charts.common.model.Point
 import co.yml.charts.ui.linechart.LineChart
-import co.yml.charts.ui.linechart.model.GridLines
 import co.yml.charts.ui.linechart.model.IntersectionPoint
 import co.yml.charts.ui.linechart.model.Line
 import co.yml.charts.ui.linechart.model.LineChartData
@@ -29,39 +31,32 @@ import co.yml.charts.ui.linechart.model.ShadowUnderLine
 import com.example.coino.feature_coins.domain.model.CoinPrices
 import com.example.coino.feature_coins.presentation.detail_screen.DetailsScreenViewModel
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
 fun LineCharts(
     prices: CoinPrices
 ) {
 
-    val points = mutableListOf<Point>()
+    val points = mutableStateListOf<Point>()
 
     prices.prices.forEach { priceList ->
         val x = priceList[0].toFloat()
-        val y = priceList[1].toFloat()
+        val y = (priceList[1].toFloat())
         points.add(Point(x, y))
     }
 
 
     val xAxisData = AxisData.Builder()
-        .axisStepSize(0.1.dp)
+        .axisStepSize(0.0001.dp)
         .backgroundColor(color = Color.Transparent)
-        .steps(prices.prices.size - 1)
-        .labelData { i -> i.toString() }
         .labelAndAxisLinePadding(15.dp)
         .axisLineColor(MaterialTheme.colorScheme.tertiary)
         .axisLabelColor(MaterialTheme.colorScheme.tertiary)
         .build()
 
     val yAxisData = AxisData.Builder()
-        .steps(10)
         .backgroundColor(Color.Transparent)
         .labelAndAxisLinePadding(20.dp)
-        .labelData { i ->
-            val yScale = 100 / 10
-            (i * yScale).toString()
-        }
-        .labelAndAxisLinePadding(15.dp)
         .axisLineColor(MaterialTheme.colorScheme.tertiary)
         .axisLabelColor(MaterialTheme.colorScheme.tertiary)
         .build()
@@ -71,7 +66,9 @@ fun LineCharts(
             lines = listOf(
                 Line(
                     dataPoints = points,
-                    LineStyle(lineType = LineType.Straight()),
+                    LineStyle(
+                        lineType = LineType.SmoothCurve()
+                    ),
                     IntersectionPoint(
                         color = MaterialTheme.colorScheme.tertiary
                     ),
@@ -92,14 +89,18 @@ fun LineCharts(
         ),
         backgroundColor = MaterialTheme.colorScheme.surface,
         xAxisData = xAxisData,
-        yAxisData = yAxisData
+        yAxisData = yAxisData,
+        isZoomAllowed = false,
+        accessibilityConfig = AccessibilityConfig(dividerColor = Color.Transparent)
 
 
     )
 
-    LineChart(modifier = Modifier
-        .fillMaxHeight()
-        .height(50.dp), lineChartData = lineChartData)
+    LineChart(
+        modifier = Modifier
+            .fillMaxHeight()
+            .height(50.dp), lineChartData = lineChartData
+    )
 
 
 }
@@ -109,10 +110,15 @@ fun RealLineChart(
     viewModel: DetailsScreenViewModel = hiltViewModel()
 ) {
 
-    val result = viewModel.state.value
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(vertical = 250.dp)) {
 
-    result.price?.let { LineCharts(prices = it) }
+        val result = viewModel.state.value
 
-    Log.d("prices", "RealLineChart: ${result.price}")
+        result.price?.let { LineCharts(prices = it) }
+        Log.d("prices", "RealLineChart: ${result.price}")
+    }
+
 
 }
